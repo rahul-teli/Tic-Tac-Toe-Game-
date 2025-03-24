15f46d0e9f19a1a58cc039e7288866b9afb7eae7
rahul-teli <rahulteli3018@gmail.com>
2025-03-24 16:53:13 +0530

    Cell Class updated

--- src/models/Cell.java
+++ src/models/Cell.java
@@ -1,4 +1,17 @@
 package models;
 
 public class Cell {
+    int row;
+    int col;
+    Player player;
+    CellState cellstate;
+
+    public Cell(int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
+
+    public void updateCell(Player player){
+
+    }
 }
